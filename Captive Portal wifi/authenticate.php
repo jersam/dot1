3cb7216cc83249cd95dc997298d3493c06b621a5
jersam <jeremysamler@gmail.com>
2025-04-07 11:39:05 -0700

    Update authenticate.php

--- Captive Portal wifi/authenticate.php
+++ Captive Portal wifi/authenticate.php
@@ -14,13 +14,13 @@ $radius = new Radius($server, $secret);
 if ($radius->accessRequest($username, $password)) {
     // Display success message before redirecting
     echo "<html><head>
-            <meta http-equiv='refresh' content='3;url=https://search.blackmesalabs.net'>
+            <meta http-equiv='refresh' content='3;url=https://search.somedomain.net'>
           </head><body>
           <div style='display: flex; justify-content: center; align-items: center; height: 100vh; text-align: center; font-family: Arial, sans-serif;'>
               <div>
                   <h2 style='color: #4A00E0;'>Authentication Successful!</h2>
                   <p style='color: #333;'>You will be redirected shortly...</p>
-                  <p>If you are not redirected, <a href='https://search.blackmesalabs.net'>click here</a>.</p>
+                  <p>If you are not redirected, <a href='https://search.somedomain.net'>click here</a>.</p>
               </div>
           </div>
           </body></html>";    exit();
